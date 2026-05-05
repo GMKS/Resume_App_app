db890a7a1d5ec26e571b9d8e1d9a8e8f325fca21
GMK Seenai <seenai07@yahoo.co.in>
2026-05-05 23:33:59 +0530

    app info file

--- lib/core/constants/app_info.dart
+++ lib/core/constants/app_info.dart
@@ -4,10 +4,11 @@ class AppInfo {
   static const String appName = 'Resumix AI';
   static const String supportEmail = 'support@resumebuilder.app';
   static const String legalEmail = 'legal@resumebuilder.app';
-  static const String websiteUrl = 'https://resumebuilder.app';
-  static const String privacyPolicyUrl = 'https://resumebuilder.app/privacy';
-  static const String userGuideUrl = 'https://resumebuilder.app/guide';
-  static const String playStorePackageId = 'com.seenaigmk.resumebuilder';
+  static const String websiteUrl = 'https://gmks.github.io/Resume_App_app/';
+  static const String privacyPolicyUrl =
+      'https://gmks.github.io/Resume_App_app/privacy/';
+  static const String userGuideUrl = 'https://gmks.github.io/Resume_App_app/guide/';
+  static const String playStorePackageId = 'com.seenaigmk.resumebuilderai';
   static const String playStoreUrl =
       'https://play.google.com/store/apps/details?id=$playStorePackageId';
 }
